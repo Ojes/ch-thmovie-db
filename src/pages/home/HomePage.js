b2d356e7c3f8c46299bcd64e933d375caaba2d0c
Ojes Otrebla <ojesotrebla@yahoo.com>
2022-02-03 19:49:18 -0300

    ref: integration popular movies and search

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,12 +1,22 @@
+import { Suspense } from 'react';
+import { useAtomValue } from 'jotai/utils';
+import SearchResultMovies from './components/SearchResultMovies';
+import PopularMovies from './components/PopularMovies';
 import SearchBox from './components/SearchBox';
+import { searchTextAtom } from '../../atoms/SearchAtoms';
 import Header from './components/Header';
+import ContainerWrapper from '../../components/Container';
 
 const HomePage = () => {
+  const searchText = useAtomValue(searchTextAtom);
   return (
     <>
       <Header>
         <SearchBox />
       </Header>
+      <ContainerWrapper>
+        <Suspense fallback="...loading">{searchText ? <SearchResultMovies /> : <PopularMovies />}</Suspense>
+      </ContainerWrapper>
     </>
   );
 };
