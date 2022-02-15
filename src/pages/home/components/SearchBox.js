d76a4b06d5cafc694f79d5893be43ef684a970c6
Ojes Otrebla <ojesotrebla@yahoo.com>
2022-02-15 12:27:13 -0300

    refactor: add submit button at search box

--- src/pages/home/components/SearchBox.js
+++ src/pages/home/components/SearchBox.js
@@ -2,11 +2,27 @@ import { useUpdateAtom } from 'jotai/utils';
 import { useState } from 'react';
 import styled from 'styled-components';
 import { requestParamsAtom } from '../../../atoms/MovieResultsAtom';
+import Button from '../../../components/Button';
 
 const Search = styled.input`
   border: none;
-  border-radius: 5px;
+  border-radius: 5px 0 0 5px;
   padding: 10px 16px;
+`;
+
+const FormWrapper = styled.form`
+  display: flex;
+  align-items: stretch;
+`;
+
+const SubmitWrapper = styled(Button)`
+  background-color: #38b8d9;
+  border-radius: 0 5px 5px 0;
+  color: white;
+  margin: 0;
+  &:disabled {
+    background-color: gray;
+  }
 `;
 
 const SearchBox = () => {
@@ -20,9 +36,12 @@ const SearchBox = () => {
 
   // TODO: dispatch event after Input
   return (
-    <form onSubmit={handleSubmit}>
-      <Search type="text" name="search" value={inputValue} onInput={(evt) => setInput(evt.target.value)} placeholder="Search movie" />
-    </form>
+    <FormWrapper onSubmit={handleSubmit}>
+      <Search type='text' name='search' value={inputValue} onInput={(evt) => setInput(evt.target.value)} placeholder='Search movie' />
+      <SubmitWrapper type='submit' disabled={!inputValue}>
+        Search
+      </SubmitWrapper>
+    </FormWrapper>
   );
 };
 
